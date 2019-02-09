9cd0cf6e8abac06e4a3fa2fcb7064087e824991c
Ian Theis <iandbtheis@gmail.com>
2019-02-09 16:32:02 -0600

    most major changes completed

--- word-guess/assets/javascript/javascript.js
+++ word-guess/assets/javascript/javascript.js
@@ -1,43 +1,132 @@
-wordBank=["test", "magic"];
-gamesWon=0
-gamesLost=0
-word=wordBank[Math.floor(Math.random()*wordBank.length)];
-guessed=[]
-guessedWrong=[]
-guessedRight=[]
-chances=6
-numWrong=0
-gameOver=false;
+var wordBank = ["test", "magic"];
+var hangManImages = ["assets/image/hangman0.png",
+    "assets/image/hangman1.png",
+    "assets/image/hangman2.png",
+    "assets/image/hangman3.png",
+    "assets/image/hangman4.png",
+    "assets/image/hangman5.png",
+    "assets/image/hangman6.png"];
+
+var game = {
+
+    won: 0,
+    lost: 0,
+    word: wordBank[Math.floor(Math.random() * wordBank.length)],
+    guessed: [],
+    guessedWrong: [],
+    guessedRight: [],
+    chances: 6,
+    numWrong: 0,
+    gameOver: false,
+    displayWord: [],
+    displayHangMan: function (number) {
+        document.getElementById("hangMan").setAttribute("src", hangManImages[number])
+    },
+    newGame: function () {
+        this.word = wordBank[Math.floor(Math.random() * wordBank.length)];
+        this.guessed = [];
+        this.guessedRight = [];
+        this.guessedWrong = [];
+        this.numWrong = 0;
+        this.gameOver = false;
+        this.resetDisplayWord();
+        this.displayHangMan(0);
+        this.showGamesLost();
+        this.showGamesWon();
+        this.showDisplayWord();
+        this.showGuesses();
+    },
 
 
-document.onkeyup = function(event) {
-    var key=event.key.toLowerCase();
-    console.log(word);
-    console.log(key);
-    if(word.includes(key)){
-        guessed.push(key);
-        guessedRight.push(key);
-        console.log(guessed);
-        console.log(guessedRight);
-    }
-    else if (guessed.includes(key)){
-        console.log('duplicate guess');
+
+    resetDisplayWord: function () {
+        for (i = 0; i < this.word.length; i++) {
+            this.displayWord[i] = '_'
+        }
+    },
+
+    updateDisplayWord: function () {
+        for (i = 0; i < this.word.length; i++) {
+            if (this.guessedRight.includes(this.word[i])) {
+                this.displayWord[i] = this.word[i];
+            }
+        }
+    },
+
+    showDisplayWord: function () {
+        document.getElementById("wordClue").innerText = this.displayWord
+    },
+
+    showGamesWon: function () {
+        document.getElementById("gamesWon").innerText = "Games Won: " + this.won
+    },
+
+    showGamesLost: function () {
+        document.getElementById("gamesLost").innerText = "Games Lost: " + this.lost
+    },
+
+    showGuesses: function (){
+        document.getElementById("guesses").innerText = "Guessed: " + this.guessed
     }
 
-    else{
-        guessed.push(key);
-        guessedWrong.push(key);
-        numWrong++
-        console.log(guessed);
-        console.log(guessedWrong);
-        console.log(numWrong + " Wrong Guess")
-        
+
+}
+
+game.displayHangMan(0);
+game.resetDisplayWord();
+game.showDisplayWord();
+game.showGamesWon();
+game.showGamesLost();
+game.showGuesses();
+
+
+document.onkeyup = function (event) {
+    if (game.gameOver) {
+        game.newGame();
+    }
+    else {
+        var key = event.key.toLowerCase();
+        console.log(game.word);
+        console.log(key);
+        if (game.word.includes(key)) {
+            game.guessed.push(key);
+            game.guessedRight.push(key);
+            console.log(game.guessed);
+            console.log(game.guessedRight);
+            game.updateDisplayWord();
+            game.showDisplayWord();
+            game.showGuesses();
+            console.log(game.displayWord);
+        }
+        else if (game.guessed.includes(key)) {
+            console.log('duplicate guess');
+        }
+
+        else {
+            game.guessed.push(key);
+            game.guessedWrong.push(key);
+            game.numWrong++;
+            game.displayHangMan(game.numWrong);
+            game.showGuesses();
+            console.log(game.guessed);
+            console.log(game.guessedWrong);
+            console.log(game.numWrong + " Wrong Guess");
+
+        }
+
+        if (game.numWrong >= game.chances) {
+            game.gameOver = true;
+            game.lost++;
+            game.showGamesLost();
+            console.log("gameover " + game.gameOver)
+            console.log("games lost " + game.lost)
+        }
+
+        else if (!(game.displayWord.includes("_"))){
+            game.gameOver = true;
+            game.won++;
+            game.showGamesWon();
+        }
     }
 
-    if (numWrong >= chances){
-        gameOver=true
-        gamesLost++
-        console.log("gameover " + gameOver)
-        console.log("games lost " + gamesLost)
-    }
 }
