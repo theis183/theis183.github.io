c5071ea91930741330aa00e346ae80610abc3c8b
Ian Theis <iandbtheis@gmail.com>
2019-02-27 16:52:21 -0600

    added timer and new game button, commented code

--- trivia/assets/javascript/trivia.js
+++ trivia/assets/javascript/trivia.js
@@ -1,3 +1,7 @@
+//will be interval, set global so multiple function can access
+var clock
+
+//all questions to be asked saved as objects with the possible answers and correct answer
 var question1 = {
     question: "The unit of measurement Astronotical Unit (AU) measures what?",
     possibleAnswers: ["The distance from Earth to the Sun", "The distance from Earth to the Moon", "The distance from Earth to Mars", "The distance from Earth to Alpha Centauri"],
@@ -47,13 +51,18 @@ var question8 = {
 }
 
 var game = {
+    //array with all question objects
     questions: [question1, question2, question3, question4, question5, question6, question7, question8],
+    //variables that contain display information and which question we are on
     numberCorrect: 0,
     numberWrong: 0,
     currentQuestion: 0,
+    timer: 15,
+    //resets DOM and then displays question and possible answers
     displayQuestion: function () {
         console.log("in displayQuestion")
         var myQuestion = this.questions[this.currentQuestion]
+        this.timerStart()
         console.log("myQuestion is " + myQuestion)
         $("#question").html(myQuestion.question)
         $("#answers").html('')
@@ -66,6 +75,7 @@ var game = {
         }
         $("#answers").append('<button type="button" class="btn btn-primary" id="submitAnswerBtn">Submit</button>')
     },
+    //removes question and answers and display a correct giphy
     displayCorrectAnswerBanner(){
         $.ajax({
             url:"https://api.giphy.com/v1/gifs/search?apikey=4Bh49w1RjN3aa88cRyxeNpd7ppM0htYQ&q=thats+correct&limit=1",
@@ -77,6 +87,7 @@ var game = {
 
         })
     },
+    //removes question and answers and display an incorrect giphy
     displayWrongAnswerBanner(){
         $.ajax({
             url:"https://api.giphy.com/v1/gifs/search?apikey=4Bh49w1RjN3aa88cRyxeNpd7ppM0htYQ&q=wrong+answer&limit=1",
@@ -89,6 +100,7 @@ var game = {
         })
         
     },
+    //removes question and answers and displays how many questions the user got correct
     displayStats(){
         $("#question").html("")
         $("#answers").html("")
@@ -103,14 +115,63 @@ var game = {
 
 
     },
-    timeUntilDisplayStats(){
-        setTimeout(function(){game.displayStats()}, 2000)
+//removes question and answers and display a times up giphy
+    displayTimesUpBanner(){
+        $.ajax({
+            url:"https://api.giphy.com/v1/gifs/search?apikey=4Bh49w1RjN3aa88cRyxeNpd7ppM0htYQ&q=times+up&limit=1",
+            method: "GET"
+        }).then(function (response){
+            $("#question").html("")
+            $("#answers").html("")
+            $("#display").html('<img src="' + response.data[0].images.original.url +'" >')
+
+        })
+        
     },
+
+    //starts timer and resets to 15
+    timerStart(){
+        clock = setInterval(this.updateTimer , 1000)
+        this.timer = 15
+        $("#timer").html(this.timer)
+    },
+//stops timer and removes from DOM
+    timerStop(){
+        clearInterval(clock)
+        $("#timer").html("")
+        console.log("Stopping Timer")
+    },
+//decrement timer and handles time up logic
+    updateTimer(){
+        game.timer--
+        $("#timer").html(game.timer)
+        console.log("Updated Timer to " + game.timer)
+        //time up logic
+        if(game.timer == 0) { 
+            game.timerStop()
+            game.displayTimesUpBanner()
+            game.numberWrong++
+            game.currentQuestion++
+            if(game.currentQuestion < game.questions.length){
+                game.timeUnitNextQuestion()
+            }
+            else{game.timeUntilDisplayStats()}
+
+        }
+    },
+
+//sets the interval to display giphy then proceeds to display stats
+    timeUntilDisplayStats(){
+        setTimeout(function(){game.displayStats()}, 2500)
+    },
+//sets the interval to display giphy then proceeds to next questtion    
     timeUnitNextQuestion(){
-        setTimeout(function (){game.displayQuestion()}, 2000)
+        setTimeout(function (){game.displayQuestion()}, 2500)
 
     },
+//checks the answer then calls the correct banner to display, also determines if all the questions have been asked and stops timer    
     checkAnswer(submitted){
+        this.timerStop()
         var myQuestion = this.questions[this.currentQuestion]
         if (submitted === myQuestion.correctAnswer) {
             this.numberCorrect++
@@ -127,6 +188,7 @@ var game = {
         else{this.timeUntilDisplayStats()}
 
     },
+//checks if the user has checked a  box, if has calls submit question    
     submitAnswer: function () {    
             var found = false
             var answerToSubmit
@@ -143,8 +205,11 @@ var game = {
 
     }
 }
-
-game.displayQuestion()
+//initiates the start of the game
+$(document).on("click", "#newGame", function(){
+    game.displayQuestion()
+})
+//handles submit button
 $(document).on("click", "#submitAnswerBtn", function () {
     game.submitAnswer()})
 
