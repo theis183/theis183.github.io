885e08a87175a60c1fc0afab5f6965e0c5ca8b6c
Ian Theis <iandbtheis@gmail.com>
2019-06-09 18:07:46 -0500

    Updates

---
+++ precache-manifest.2ed03c8f43404bd43d55c4ce01d32cc3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca4b797a765201049e52983b7bd2e7c5",
+    "url": "/click-game/index.html"
+  },
+  {
+    "revision": "a3ede4b793bf6b731443",
+    "url": "/click-game/static/css/main.482b3a77.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/click-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "a3ede4b793bf6b731443",
+    "url": "/click-game/static/js/main.ae647586.chunk.js"
+  },
+  {
+    "revision": "aa765b97c1e7aaa53c3e",
+    "url": "/click-game/static/js/runtime~main.7d043b49.js"
+  }
+]);
